c4c0933780b9bf8e56a6a5caf494188453b7ab85
JairoQC <jairoqc.02@gmail.com>
2025-04-09 14:16:13 -0500

    modelo

--- recetas/core/models.py
+++ recetas/core/models.py
@@ -1,3 +1,15 @@
-from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Receta(models.Model):
+    titulo = models.CharField(max_length=200)
+    descripcion = models.TextField()
+    ingredientes = models.TextField()
+    pasos = models.TextField()
+    tiempo_preparacion = models.IntegerField(help_text="Minutos")
+    imagen = models.ImageField(upload_to='recetas/', null=True, blank=True)
+    creado = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.titulo
