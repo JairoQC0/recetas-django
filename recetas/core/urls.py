6f0560f879092da24e827d65eed449fa24b3a6a2
JairoQC <jairoqc.02@gmail.com>
2025-04-09 14:42:30 -0500

    views with edit and delete

--- recetas/core/urls.py
+++ recetas/core/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.lista_recetas, name='lista_recetas'),
     path('crear/', views.crear_receta, name='crear_receta'),
-
+    path('receta/<int:receta_id>/', views.detalle_receta, name='detalle_receta'),
+    path('receta/<int:receta_id>/editar/', views.editar_receta, name='editar_receta'),
+    path('receta/<int:receta_id>/eliminar/', views.eliminar_receta, name='eliminar_receta'),
 ]
--- recetas/core/views.py
+++ recetas/core/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import RecetaForm
 from .models import Receta
 
@@ -15,3 +15,26 @@ def crear_receta(request):
     else:
         form = RecetaForm()
     return render(request, 'core/crear_receta.html', {'form': form})
+
+def detalle_receta(request, receta_id):
+    receta = get_object_or_404(Receta, id=receta_id)
+    return render(request, 'core/detalle_receta.html', {'receta': receta})
+
+def editar_receta(request, receta_id):
+    receta = get_object_or_404(Receta, id=receta_id)
+    if request.method == 'POST':
+        form = RecetaForm(request.POST, request.FILES, instance=receta)
+        if form.is_valid():
+            form.save()
+            return redirect('detalle_receta', receta_id=receta.id)
+    else:
+        form = RecetaForm(instance=receta)
+    return render(request, 'core/editar_receta.html', {'form': form, 'receta': receta})
+
+def eliminar_receta(request, receta_id):
+    receta = get_object_or_404(Receta, id=receta_id)
+    if request.method == 'POST':
+        receta.delete()
+        return redirect('lista_recetas')
+    return render(request, 'core/eliminar_receta.html', {'receta': receta})
+
